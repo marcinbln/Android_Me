558a0bafec88e4c85299dd4c3d7db251e1d8b158
marcinbln <marcinbjl@gmail.com>
2018-01-29 20:06:08 +0000

    test

--- app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
+++ app/src/main/java/com/example/android/android_me/ui/AndroidMeActivity.java
@@ -16,8 +16,9 @@
 
 package com.example.android.android_me.ui;
 
-import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.support.v4.app.FragmentManager;
+import android.support.v7.app.AppCompatActivity;
 
 import com.example.android.android_me.R;
 
@@ -25,12 +26,12 @@ import com.example.android.android_me.R;
 public class AndroidMeActivity extends AppCompatActivity {
 
     // TODO (1) Create a layout file that displays one body part image named fragment_body_part.xml
-        // This layout should contain a single ImageView
+    // This layout should contain a single ImageView
 
     // TODO (2) Create a new class called BodyPartFragment to display an image of an Android-Me body part
-        // In this class, you'll need to implement an empty constructor and the onCreateView method
-        // TODO (3) Show the first image in the list of head images
-            // Soon, you'll update this image display code to show any image you want
+    // In this class, you'll need to implement an empty constructor and the onCreateView method
+    // TODO (3) Show the first image in the list of head images
+    // Soon, you'll update this image display code to show any image you want
 
 
 
@@ -40,5 +41,15 @@ public class AndroidMeActivity extends AppCompatActivity {
         setContentView(R.layout.activity_android_me);
 
         // TODO (5) Create a new BodyPartFragment instance and display it using the FragmentManager
+        BodyPartFragment headPartFragment = new BodyPartFragment();
+
+        FragmentManager fragmentManager = getSupportFragmentManager();
+
+        fragmentManager.beginTransaction()
+                .add(R.id.head_container, headPartFragment)
+                .commit();
+
+
+
     }
 }
